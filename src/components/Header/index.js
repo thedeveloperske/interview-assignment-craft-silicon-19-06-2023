990f739fecfe3eac66275236b5012a09a4f43b74
TheDevelopersKE <thedeveloperske@gmail.com>
2023-06-21 09:13:50 +0300

    HeaderComponent

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,18 @@
 import * as React from "react";
 
+//client types
+const clientTypes = ["Enterprise", "Governent Institution", "Limited Company"];
+
+//segment types
+const segmentTypes = ["Corporate", "Individual", "SME"];
+
+//sub segment types
+const subSegmentTypes = [
+  "Public Limited Company",
+  "Sole Business",
+  "Private Limited Company",
+];
+
 export default function Header() {
   return (
     <div>
@@ -15,12 +28,99 @@ export default function Header() {
                   <div className="x_panel">
                     <div className="x_title">
                       <h2>
-                        <i className="fa fa-bars"></i> Summary{" "}
-                        <small>Summary Details</small>
+                        Basic Data <small>Search & View Data</small>
                       </h2>
                       <div className="clearfix"></div>
                     </div>
-                    <div className="x_content"></div>
+                    <div class="form-group row">
+                      <div class="col-md-4 left-margin">
+                        <form class="form-horizontal form-label-left">
+                          <div class="form-group row">
+                            <label>Client ID</label>
+                            <input
+                              type="text"
+                              class="form-control"
+                            />
+                          </div>
+                          <div class="form-group row">
+                            <label>Client Type</label>
+                            <select
+                              class="select2_single form-control"
+                              tabindex="-1"
+                              disabled
+                            >
+                              <option>Enterprise</option>
+                              {clientTypes.map((clientType) => (
+                                <option value={clientType}>
+                                  {" "}
+                                  {clientType}
+                                </option>
+                              ))}
+                            </select>
+                          </div>
+                        </form>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-4 left-margin">
+                          <form class="form-horizontal form-label-left">
+                            <div class="form-group row">
+                              <label>Segment Type</label>
+                              <select
+                                class="select2_single form-control"
+                                tabindex="-1"
+                                disabled
+                              >
+                                <option>Corporate</option>
+                                {segmentTypes.map((segmentType) => (
+                                  <option value={segmentType}>
+                                    {" "}
+                                    {segmentType}
+                                  </option>
+                                ))}
+                              </select>
+                            </div>
+                            <div class="form-group row">
+                              <label>Sub Segment Type</label>
+                              <select
+                                class="select2_single form-control"
+                                tabindex="-1"
+                              >
+                                <option>Public Limited Company</option>
+                                {subSegmentTypes.map((subSegmentType) => (
+                                  <option value={subSegmentType}>
+                                    {" "}
+                                    {subSegmentType}
+                                  </option>
+                                ))}
+                              </select>
+                            </div>
+                          </form>
+                        </div>
+                      </div>
+                      <div class="col-md-12 left-margin">
+                        <form class="form-horizontal form-label-left">
+                          <div class="form-group row">
+                            <label>Company Name</label>
+                            <input
+                              disabled
+                              type="text"
+                              class="form-control"
+                            />
+                          </div>
+                        </form>
+                      </div>
+                      <div class="col-md-12 left-margin">
+                        <form class="form-horizontal form-label-left">
+                          <div class="form-group row">
+                            <label>Base ID</label>
+                            <input
+                              type="text"
+                              class="form-control"
+                            />
+                          </div>
+                        </form>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
